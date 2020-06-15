858023cd5bfaf199d3df7a8dc5fbfd5c8c3fdefb
joe <joe@joes-MacBook-Pro.local>
2020-06-15 10:21:17 +0800

    support visit the page in browser or headless

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import Report from './screens/Report';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://web-server/api/user';
-// axios.defaults.baseURL = 'http://localhost:8080/api/user';
 
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 const params = new URLSearchParams(window.location.search);
 const date = params.get('date');
+const open_in = params.get('open_in');
+
+if (open_in === 'browser') {
+  axios.defaults.baseURL = 'https://hkcic.creaxtive.com/api/user';
+} else {
+  axios.defaults.baseURL = 'http://web-server/api/user';
+}
+
 // console.log(date);
 document.title = `Report ${date}`;
 
